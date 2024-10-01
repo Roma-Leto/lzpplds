371a684bec020ba73896a00be1392a9975e89ad3
Roma-Leto <119625441+Roma-Leto@users.noreply.github.com>
2024-10-02 00:36:32 +0300

    add base.html, home page (html + view), css

--- lzpplds/urls.py
+++ lzpplds/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from lzpplapp.views import *
 
 urlpatterns = [
+    path('', HomePage.as_view(), name="home_page"),
     path('admin/', admin.site.urls),
+    path('accounts/', include('users.urls', namespace='users_app_url')),
 ]
